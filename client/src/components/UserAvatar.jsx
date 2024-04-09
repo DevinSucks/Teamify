751e589f866a5e7053a5f250267b17cdaf87c5fa
harshit-dev-ui <65815998+harshit-dev-ui@users.noreply.github.com>
2024-04-09 23:54:14 +0530

    add sidebar navbar

---
+++ client/src/components/UserAvatar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function UserAvatar() {
+  return (
+    <div className="h-[35px] w-[35px] border border-solid rounded-full bg-slate-500">
+      {" "}
+    </div>
+  );
+}
+
+export default UserAvatar;
